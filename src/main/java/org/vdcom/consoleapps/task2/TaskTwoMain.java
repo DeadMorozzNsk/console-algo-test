856f9acf1cb27e058a9358ad9ef2660f66120c7a
DeadMorozz <evlampejev@gmail.com>
2020-12-10 15:23:17 +0400

    initial commit

---
+++ src/main/java/org/vdcom/consoleapps/task2/TaskTwoMain.java
@@ -0,0 +1,7 @@
+package org.vdcom.consoleapps.task2;
+
+public class TaskTwoMain {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/vdcom/consoleapps/task3/TaskThreeMain.java
@@ -0,0 +1,7 @@
+package org.vdcom.consoleapps.task3;
+
+public class TaskThreeMain {
+    public static void main(String[] args) {
+
+    }
+}
